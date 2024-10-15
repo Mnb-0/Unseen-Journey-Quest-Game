deffcc4dcc70ed550f5b873d3a9b53995979f1b7
Muneeb <150353438+Mnb-0@users.noreply.github.com>
2024-10-15 18:05:38 +0500

    Created Cell Class
    Added Cell (Node) class to main.cpp
    Code should not be executed right now

--- main.cpp
+++ main.cpp
@@ -1,10 +1,96 @@
+#include <iostream>
 #include <ncurses.h>
 
-int main() {
-    initscr();               // Start ncurses mode
-    printw("Hello, ncurses!"); // Print Hello, World
-    refresh();               // Print it on to the real screen
-    getch();                 // Wait for user input
-    endwin();                // End ncurses mode
+using namespace std;
+
+class Cell
+{
+public:
+    Cell *right;
+    Cell *left;
+    Cell *up;
+    Cell *down;
+    char data;
+/* data used to represent different items:
+ * '.' - empty space
+ * '#' - wall
+ * 'P' - player
+ * 'B' - bomb
+ * 'E' - exit
+ * 'C' - coin
+ */
+    Cell()
+    {
+        right = nullptr;
+        left = nullptr;
+        up = nullptr;
+        down = nullptr;
+        data = '.';
+    }
+
+    Cell(char data)
+    {
+        right = nullptr;
+        left = nullptr;
+        up = nullptr;
+        down = nullptr;
+        this -> data = data;
+    }
+
+    void moveRight()
+    {
+        if(right != nullptr)
+        {
+            data = '.';
+            right -> data = 'P';
+        }
+    }
+
+    void moveLeft()
+    {
+        if(left != nullptr)
+        {
+            data = '.';
+            left -> data = 'P';
+        }
+    }
+
+    void moveUp()
+    {
+        if(up != nullptr)
+        {
+            data = '.';
+            up -> data = 'P';
+        }
+    }
+
+    void moveDown()
+    {
+        if(down != nullptr)
+        {
+            data = '.';
+            down -> data = 'P';
+        }
+    }
+
+    bool checkEdge() const
+    {
+        if(right == nullptr || left == nullptr || up == nullptr || down == nullptr)
+        {
+            return true;
+        }
+        return false;
+    }
+
+
+};
+
+int main()
+{
+    initscr(); // Start ncurses mode
+    // printw("Hello!");
+    // refresh();               // Print it on to the real screen
+    // getch();                 // Wait for user input
+    endwin(); // End ncurses mode
     return 0;
 }
