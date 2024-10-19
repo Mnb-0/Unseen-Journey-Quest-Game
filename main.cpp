4099c35e3ffb26ff20409a994fa4bff466d552e5
Muneeb <150353438+Mnb-0@users.noreply.github.com>
2024-10-19 22:45:48 +0500

    Added basic Player class

--- main.cpp
+++ main.cpp
@@ -7,7 +7,43 @@
 
 using namespace std;
 
-int main() {
+class Player
+{
+    Cell *playerCurrent;
+    Cell *playerPrev; // To make undos easier
+    bool hasKey;
+    bool score;
+    bool alive;
+    int moves;
+    int undos;
+
+public:
+    Player(Cell *start=nullptr) : playerCurrent(start), playerPrev(nullptr), hasKey(false), score(false), alive(true), moves(0), undos(0) {}
+
+    void setKey(bool hasKey) { this->hasKey = hasKey; }
+    bool getKey() { return this->hasKey; }
+
+    void setScore(bool score) { this->score = score; }
+    void incrementScore() { this->score += 20; }
+    bool getScore() { return this->score; }
+
+    void setAlive(bool alive) { this->alive = alive; }
+    bool getAlive() { return this->alive; }
+
+    void setMoves(int moves) { this->moves = moves; }
+    void incrementMoves() { this->moves++; }
+    void decrementMoves() { this->moves--; }
+    int getMoves() { return this->moves; }
+
+    void setUndos(int undos) { this->undos = undos; }
+    void incrementUndos() { this->undos++; }
+    void decrementUndos() { this->undos--; }
+    int getUndos() { return this->undos; }
+
+
+};
+int main()
+{
     srand(static_cast<unsigned>(time(0)));
 
     Maze maze;
